8a2235f3dbe98d57a8f7014b0feead22ac9304a0
Selena <selena.poun@gmail.com>
2022-01-19 22:49:11 +0100

    update css

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,41 +3,51 @@ import Link from "next/link";
 import Image from "next/image";
 import LogoManoMano from "../public/Img/LogoManoMano.png";
 import { CgShoppingCart } from "react-icons/cg";
-import tailwindHamburgers from "tailwind-hamburgers";
 
 export default function Header() {
   return (
-    <div className="bg-slate-50 justify-between">
-      <div className="flex flex-row mb-4">
-        <Link href="/">
-          <a className="flex justify-start sm:justify-start">
-            <Image src={LogoManoMano} alt="Logo" width="105px" height="34px" />
-          </a>
-        </Link>
-        <p className="flex justify-end sm:justify-end text-slate-600 text-sm mr-4">
-          Identifiez-vous
-        </p>
-        <CgShoppingCart />
-      </div>
-      <div className="ml-2 tham tham-e-squeeze tham-w-6">
-        <div className="tham-box">
-          <div className="tham-inner" />
+    <header className="min-h-screen bg-gray-50">
+      <nav className="flex justify-between p-2 items-center">
+        <div className="flex items-center space-x-2">
+          <Link href="/">
+            <a className="sm:inline-block text-gray-700 cursor-pointer">
+              <Image
+                src={LogoManoMano}
+                alt="Logo"
+                width="105px"
+                height="34px"
+              />
+            </a>
+          </Link>
         </div>
-      </div>
-      {/* <div className=" bg-gray-200">
-        <div className="container h-screen flex justify-center items-center px-4 sm:px-6 lg:px-8">
-          <div className="relative">
-            <input
-              type="text"
-              className="h-9 w-60 pr-8 pl-5 rounded z-0 focus:shadow focus:outline-none"
-              placeholder="Rechercher un produit..."
-            />
-            <div className="absolute top-4 right-3">
-              <i className="fa fa-search text-gray-400 z-20 hover:text-gray-500"></i>{" "}
+        <div className="flex items-center space-x-8">
+          <p className=" text-slate-600 text-sm mr-4 cursor-pointer">
+            Identifiez-vous
+          </p>
+          <CgShoppingCart />
+        </div>
+        <div className="flex items-center space-x-2">
+          <div className="ml-2 tham tham-e-squeeze tham-w-6">
+            <div className="tham-box">
+              <div className="tham-inner" />
             </div>
           </div>
         </div>
-      </div> */}
-    </div>
+        {/*    <div className=" bg-gray-200">
+          <div className="container h-screen flex justify-center items-center px-4 sm:px-6 lg:px-8">
+            <div className="relative">
+              <input
+                type="text"
+                className="h-9 w-60 pr-8 pl-5 rounded z-0 focus:shadow focus:outline-none"
+                placeholder="Rechercher un produit..."
+              />
+              <div className="absolute top-4 right-3">
+                <i className="fa fa-search text-gray-400 z-20 hover:text-gray-500"></i>{" "}
+              </div>
+            </div>
+          </div>
+        </div> */}
+      </nav>
+    </header>
   );
 }
