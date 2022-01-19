dd589e264383d097ce6e3084569bfbcdf3cbc254
arthaud14 <77742592+arthaud14@users.noreply.github.com>
2022-01-19 21:43:35 +0100

    Add movie

--- pages/product.js
+++ pages/product.js
@@ -6,8 +6,23 @@ import { BsStarFill, BsStar, BsPaypal } from "react-icons/bs";
 import { FaCcVisa, FaCcMastercard } from "react-icons/fa";
 import { GiPadlock } from "react-icons/gi";
 import ReactPlayer from "react-player";
+import React, { useState } from "react";
+
+
+    
+
 
 export default function Product() {
+
+    const [count, setCount] = useState(0);
+
+    const increment = (event) => {
+        setCount(count + 1);
+    }
+    const decrement = () => {
+        setCount(count - 1);
+    }
+
   return (
     <div>
       <div className="flex flex-wrap items-center justify-center">
@@ -69,24 +84,24 @@ export default function Product() {
             <div className="prod-title">
               <div className="custom-number-input h-10 w-32">
                 <label
-                  for="custom-input-number"
+                  for="custom-input-number" 
                   className="w-full text-gray-700 text-sm font-semibold"
                 ></label>
                 <div className="flex flex-row h-10 w-full rounded-lg relative bg-transparent mt-1">
                   <button
-                    data-action="decrement"
+                    onClick={decrement}
                     className=" bg-gray-300 text-gray-600 hover:text-gray-700 hover:bg-gray-400 h-full w-20 rounded-l cursor-pointer outline-none"
                   >
                     <span className="m-auto text-2xl font-thin">−</span>
                   </button>
                   <input
-                    type="number"
+                    type="number"  min="0" step="1" value={count && Math.max(0, count)}
                     className="outline-none focus:outline-none text-center w-full bg-gray-300 font-semibold text-md hover:text-black focus:text-black  md:text-basecursor-default flex items-center text-gray-700  outline-none"
                     name="custom-input-number"
-                    value="0"
+                    
                   ></input>
                   <button
-                    data-action="increment"
+                    onClick={increment}
                     className="bg-gray-300 text-gray-600 hover:text-gray-700 hover:bg-gray-400 h-full w-20 rounded-r cursor-pointer"
                   >
                     <span className="m-auto text-2xl font-thin">+</span>
