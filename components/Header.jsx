d7c2802260596be662b2c90221ebd4e0a2356741
Selena <selena.poun@gmail.com>
2022-01-20 11:43:58 +0100

    navbar not works

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,54 +3,54 @@ import Link from "next/link";
 import Image from "next/image";
 import LogoManoMano from "../public/Img/LogoManoMano.png";
 import { CgShoppingCart } from "react-icons/cg";
+import { VscSearch } from "react-icons/vsc";
 
 export default function Header() {
   return (
-    <header className=" bg-gray-50">
-      <nav className="flex justify-between p-2 items-center">
-        <div className="flex-inline">
-          <Link href="/">
-            <a className="sm:inline-block text-gray-700 cursor-pointer">
-              <Image
-                src={LogoManoMano}
-                alt="Logo"
-                width="105px"
-                height="34px"
-              />
+    <nav className="">
+      <span className="flex justify-between">
+        <Link href="/">
+          <a>
+            <Image src={LogoManoMano} alt="Logo" width="105px" height="34px" />
+          </a>
+        </Link>
+
+        <div className="flex justify-between items-center mt-10">
+          <Link href="/userCompte" passHref>
+            <a>
+              <p className="text-slate-600 text-sm mr-4 ">Identifiez-vous</p>
+              <CgShoppingCart />
             </a>
           </Link>
-
-          <div className="flex items-center space-x-2">
-            <div className="ml-2 tham tham-e-squeeze tham-w-6">
-              <div className="tham-box">
-                <div className="tham-inner" />
-              </div>
-            </div>
-          </div>
         </div>
-        <Link href="/userCompte">
-          <div className="flex flex-row-reverse justify-end items-end">
-            <CgShoppingCart />
-            <p className=" text-slate-600 text-sm mr-4 cursor-pointer">
-              Identifiez-vous
-            </p>
-          </div>
-        </Link>
-        {/*    <div className=" bg-gray-200">
-          <div className="container h-screen flex justify-center items-center px-4 sm:px-6 lg:px-8">
-            <div className="relative">
-              <input
-                type="text"
-                className="h-9 w-60 pr-8 pl-5 rounded z-0 focus:shadow focus:outline-none"
-                placeholder="Rechercher un produit..."
-              />
-              <div className="absolute top-4 right-3">
-                <i className="fa fa-search text-gray-400 z-20 hover:text-gray-500"></i>{" "}
-              </div>
-            </div>
-          </div>
-        </div> */}
-      </nav>
-    </header>
+      </span>
+
+      <div className="ml-2 tham tham-e-squeeze tham-w-6">
+        <div className="tham-box">
+          <div className="tham-inner" />
+        </div>
+      </div>
+
+      <div className="flex-around hover:w-1/7 group">
+        <a
+          href="#"
+          className="flex items-center justify-center text-center mx-auto px-4 py-2 group-hover:w-1/5 text-black"
+        >
+          <input
+            type="search"
+            name="search"
+            className="rounded-2xl z-10 ml-3 align-bottom p-1 pl-4 md:w-15"
+            placeholder="Rechercher un produit..."
+          />
+          <VscSearch />
+        </a>
+      </div>
+    </nav>
   );
 }
+
+// grid-template-areas:
+// "logo logo space navigation"
+// "button search search search"
+// "links links links links";
+// grid-template-columns: auto auto 1fr auto;
