e8b6182f66720ccf0ef0fff7edea25efc1d1040a
Selena <selena.poun@gmail.com>
2022-01-20 18:00:31 +0100

    navbar fix héhéhéhé

--- components/Header.jsx
+++ components/Header.jsx
@@ -9,7 +9,7 @@ import { AiOutlineMenu } from "react-icons/ai";
 export default function Header() {
   return (
     <header className=" bg-gray-50">
-      <nav className="grid overflow-hidden grid-cols-2 grid-rows-2 gap-px">
+      <nav className="grid overflow-hidden grid-cols-2 grid-rows-1 gap-px">
         <div className="box">
           <Link href="/">
             <a className="sm:inline-block text-gray-700 cursor-pointer">
@@ -23,7 +23,7 @@ export default function Header() {
           </Link>
         </div>
 
-        <div className="box justify-items-end cursor-pointer">
+        <div className="box flex justify-end cursor-pointer">
           <Link href="/userCompte" passHref>
             <a>
               <p className="text-slate-600 text-sm mr-4 flex pt-3">
@@ -32,29 +32,29 @@ export default function Header() {
             </a>
           </Link>
         </div>
+      </nav>
 
-        <div className="box ml-2">
+      <div className="flex justify">
+        <div className="ml-2 my-2">
           <AiOutlineMenu size={25} />
         </div>
+        <div className="flex-around hover:w-4/5 group">
+          <a
+            href="#"
+            className="flex items-center justify-center text-center mx-auto px-4 py-2 group-hover:w-1/5 text-black"
+          >
+            <input
+              type="search"
+              className="w-full px-4 py-1 text-gray-800 rounded-full focus:outline-none rounded-r-lg"
+              placeholder="Look for a product"
+              x-model="search"
+            />
 
-        <div className="flex justify-end">
-          <div className="flex-around hover:w-1/7 group">
-            <a
-              href="#"
-              className="flex items-center justify-center text-center mx-auto px-4 py-2 group-hover:w-1/5 text-black"
-            >
-              <input
-                type="search"
-                className="w-full px-4 py-1 text-gray-800 rounded-full focus:outline-none rounded-r-lg"
-                placeholder="search"
-                x-model="search"
-              />
-
-              <VscSearch className="rounded-r-lg bg-white w-8 h-8" />
-            </a>
-          </div>
+            <VscSearch className="rounded-r-lg bg-white w-8 h-8" />
+          </a>
         </div>
-      </nav>
+      </div>
+
       <div className="flex justify-between items-center flex-wrap text-sm mx-4">
         <p>Great deals</p>
         <p>Best seller</p>
